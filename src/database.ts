8c7f7c1e389e75acb7880250b3471dc83064dee9
PhoiNT <phoi.nt2806@gmail.com>
2024-08-03 16:03:26 +0700

    chore: install package and .gitignore

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { configDotenv } from 'dotenv';
+import { Pool } from 'pg';
+
+configDotenv();
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+  process.env;
+
+const client = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
+
+export default client;
